a903f993c44dd75291ebee1ae26d70d01e2b44eb
Adrian Miasik <AdrianMiasik@gmail.com>
2021-04-13 19:31:49 -0400

    Implement double digit prefabs

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/PomodoroTimer.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/PomodoroTimer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,15 @@ namespace AdrianMiasik
     // TODO: Clean up
     public class PomodoroTimer : MonoBehaviour
     {
+        [Header("Digits")] 
+        [SerializeField] private DoubleDigit hourDigits;
+        [SerializeField] private DoubleDigit minuteDigits;
+        [SerializeField] private DoubleDigit secondDigits;
+
         [Header("Buttons")]
         [SerializeField] private PlayPauseButton playPauseButton;
 
+        [Header("Ring")]
         [SerializeField] private Image progress;
 
         [Header("Colors")] 
@@ -16,16 +23,28 @@ namespace AdrianMiasik
         [SerializeField] private Color runningColor = new Color(0.35f, 0.89f, 0.4f);
         [SerializeField] private Color completedColor = new Color(0.97f, 0.15f, 0.15f);
 
-        public float startingTime = 15f; // in seconds
-
+        [Header("Data")]
+        [SerializeField] private int hours = 0;
+        [SerializeField] private int minutes = 0;
+        [SerializeField] private int seconds = 15;
+        
         // Cache
         private bool _isRunning;
-        private float _currentTime;
+        private double _currentTime;
         private bool _isComplete;
         
         // Specific to our ring shader
         private static readonly int RingColor = Shader.PropertyToID("Color_297012532bf444df807f8743bdb7e4fd");
-        
+
+        private float totalTimeInSeconds;
+
+        private void OnValidate()
+        {
+            hours = Mathf.Clamp(hours, 0, 99);
+            minutes = Mathf.Clamp(minutes, 0, 59);
+            seconds = Mathf.Clamp(seconds, 0, 59);
+        }
+
         private void Start()
         {
             Setup();
@@ -34,12 +53,22 @@ namespace AdrianMiasik
         private void Setup()
         {
             _isComplete = false;
+            TimeSpan ts = TimeSpan.FromHours(hours) +  TimeSpan.FromMinutes(minutes) +  TimeSpan.FromSeconds(seconds);
+            CalculateDigits(ts);
+            _currentTime = ts.TotalSeconds;
+            totalTimeInSeconds = (float) ts.TotalSeconds;
             
             Pause();
-            _currentTime = startingTime;
             progress.fillAmount = 1f;
             
             playPauseButton.Initialize(_isRunning);
+        }
+
+        private void CalculateDigits(TimeSpan timeSpan)
+        {
+            hourDigits.Initialize((int) timeSpan.TotalHours);
+            minuteDigits.Initialize(timeSpan.Minutes);
+            secondDigits.Initialize(timeSpan.Seconds);
         }
 
         private void Update()
@@ -59,7 +88,10 @@ namespace AdrianMiasik
                     return;
                 }
 
-                progress.fillAmount = _currentTime / startingTime;
+                progress.fillAmount = (float) _currentTime / totalTimeInSeconds;
+                
+                CalculateDigits(TimeSpan.FromSeconds(_currentTime));
+                
                 _currentTime -= Time.deltaTime;
             }
         }
