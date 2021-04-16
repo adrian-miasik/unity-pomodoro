b9c869845ac35ab8a64eb60294714c3ce1e3cf01
Adrian Miasik <AdrianMiasik@gmail.com>
2021-04-15 20:24:38 -0400

    Fix play pause button icon pivot

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/PlayPauseButton.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/PlayPauseButton.cs
@@ -36,6 +36,11 @@ namespace AdrianMiasik
         /// </summary>
         public void UpdateIcon()
         {
+            // Correct icon offset
+            Vector2 pivot = icon.rectTransform.pivot;
+            icon.rectTransform.pivot = timer.IsRunning() ? new Vector2(0.5f, pivot.y) : new Vector2(0.6f, pivot.y); // magic numbers
+            icon.rectTransform.offsetMin = new Vector2(0, 0);
+            
             icon.sprite = timer.IsRunning() ? pause : play;
             icon.color = timer.IsRunning() ? pauseColor : playColor;
         }
