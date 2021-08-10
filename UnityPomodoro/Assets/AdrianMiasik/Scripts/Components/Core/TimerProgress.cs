123abc1c53c18a7b221930a74c291b3a32bacb7b
Adrian Miasik <AdrianMiasik@gmail.com>
2021-08-10 04:34:09 -0400

    Implement creditsbubble & created TimerProgress.cs

---
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Core/TimerProgress.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace AdrianMiasik.Components.Core 
+{ 
+    public abstract class TimerProgress : MonoBehaviour
+    {
+        [SerializeField] protected float duration = 3f;
+        [SerializeField] private bool loop = false;
+        
+        private bool isInit; // Set this to true to run timer
+        private float progress;
+        private float elapsedTime;
+        private bool hasCompleted;
+
+        protected void Initialize(float timerDuration)
+        {
+            duration = timerDuration;
+            isInit = true;
+            hasCompleted = false;
+        }
+
+        protected void Restart()
+        {
+            Initialize(duration);
+            elapsedTime = 0;
+        }
+        
+        /// <summary>
+        /// Update loop for timer
+        /// </summary>
+        /// <param name="progress">A progress value between 0 to 1</param>
+        protected abstract void OnUpdate(float progress);
+        
+        /// <summary>
+        /// Invoked when the timer has completed
+        /// </summary>
+        protected abstract void OnComplete();
+        
+        protected virtual void Update()
+        {
+            if (!isInit || hasCompleted && !loop)
+            {
+                return;
+            }
+        
+            elapsedTime += Time.deltaTime;
+            progress = elapsedTime / duration;
+            OnUpdate(progress);
+
+            if (elapsedTime >= duration)
+            {
+                OnComplete();
+                elapsedTime = 0;
+                hasCompleted = true;
+            }
+        }
+    }
+}
--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CreditsBubble.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CreditsBubble.cs
@@ -1,31 +1,136 @@
+using AdrianMiasik.Components.Core;
 using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace AdrianMiasik.Components
 {
-    public class CreditsBubble : MonoBehaviour
+    public class CreditsBubble : TimerProgress, IPointerEnterHandler, IPointerExitHandler
+
     {
-        [SerializeField] private SVGImage icon;
-        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private SVGImage background;
+        [SerializeField] private ClickButton icon;
+        [SerializeField] private CanvasGroup textContainer;
+
+        private bool isAnimating;
+        private float elapsedTime;
+        private float fadeTime = 0.5f;
+        private float progress;
+        private Color startingColor;
+        private Color currentColor;
+        private Color targetColor;
+
+        private bool isPointerHovering;
+        private bool lockInteraction;
+
+        private enum FadeState
+        {
+            IDLE,
+            FADING_OUT,
+            FADING_IN
+        }
+
+        private FadeState state;
+
+        public void Initialize()
+        {
+            Initialize(duration);
+            Lock();
+        }
         
-        // TODO: Wait 3 seconds, fade out with alpha group
-        // TODO: Provide helper functions so the info page can toggle this credits bubble
-
-        private void Update()
+        protected override void OnUpdate(float progress)
         {
+            // Nothing
+        }
+
+        protected override void OnComplete()
+        {
+            if (!isPointerHovering)
+            {
+                FadeOut();
+            }
             
+            Unlock();
         }
 
-        [ContextMenu("Show")]
-        public void Show()
+        protected override void Update()
         {
-            canvasGroup.alpha = 1;
+            if (isAnimating)
+            {
+                if (state == FadeState.FADING_OUT || state == FadeState.FADING_IN)
+                {
+                    elapsedTime += Time.deltaTime;
+                    progress = elapsedTime / fadeTime;
+
+                    currentColor = Color.Lerp(startingColor, targetColor, progress);
+
+                    // Apply
+                    background.color = currentColor;
+                    textContainer.alpha = currentColor.a;
+
+                    if (elapsedTime >= fadeTime)
+                    {
+                        state = FadeState.IDLE;
+                        isAnimating = false;
+                    }
+                }
+            }
+
+            base.Update();
         }
 
-        [ContextMenu("Hide")]
-        public void Hide()
+        public void FadeOut()
         {
-            canvasGroup.alpha = 0;
+            state = FadeState.FADING_OUT;
+            startingColor = background.color;
+            targetColor = background.color;
+            targetColor.a = 0;
+            elapsedTime = 0;
+            isAnimating = true;
+        }
+
+        public void FadeIn()
+        {
+            state = FadeState.FADING_IN;
+            startingColor = background.color;
+            targetColor = background.color;
+            targetColor.a = 1;
+            elapsedTime = 0;
+            isAnimating = true;
+        }
+
+        public void Lock()
+        {
+            lockInteraction = true;
+        }
+
+        public void Unlock()
+        {
+            lockInteraction = false;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isPointerHovering = true;
+            
+            if (lockInteraction)
+            {
+                return;
+            }
+            
+            FadeIn();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isPointerHovering = false;
+            
+            if (lockInteraction)
+            {
+                return;
+            }
+            
+            FadeOut();
         }
     }
 }
