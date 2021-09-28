93f38a78c73f677975517f2b1190ae567946fb2f
Adrian Miasik <AdrianMiasik@gmail.com>
2021-09-27 22:21:01 -0400

    Decrease hold activation time on our clickbtns

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Core/ClickButton.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Core/ClickButton.cs
@@ -28,7 +28,7 @@ namespace AdrianMiasik.Components.Core
         // Hold
         private bool isUserHolding; // Is the user pressing down?
         private float userHoldTime; // How long has the user been holding down for?
-        private float holdActivationTime = 0.5f; // How long does the user have to hold to activate our hold click logic?
+        private float holdActivationTime = 0.25f; // How long does the user have to hold to activate our hold click logic?
         private float accumlatedHoldTime; // How long has the hold logic been running for? Not to be confused with userHoldTime.
         
         // Click sound pitch variation
