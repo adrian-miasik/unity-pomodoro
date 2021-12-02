730eb901b2f5e1b98e60502a3a031803aab14b9d
Adrian Miasik <github@adrian-miasik.com>
2021-12-01 20:51:57 -0500

    Implement partial theming to settings panel

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/LabelledDropdown.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/LabelledDropdown.cs
@@ -1,7 +1,9 @@
 using AdrianMiasik.Interfaces;
 using AdrianMiasik.ScriptableObjects;
 using TMPro;
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AdrianMiasik.Components
 {
@@ -9,6 +11,8 @@ namespace AdrianMiasik.Components
     {
         [SerializeField] private TMP_Text label;
         [SerializeField] private TMP_Dropdown dropdown;
+        [SerializeField] private Image containerOutline;
+        [SerializeField] private SVGImage arrow;
 
         private PomodoroTimer timer;
     
@@ -21,6 +25,9 @@ namespace AdrianMiasik.Components
         public void ColorUpdate(Theme _theme)
         {
             label.color = _theme.GetCurrentColorScheme().foreground;
+            dropdown.captionText.color = _theme.GetCurrentColorScheme().foreground;
+            containerOutline.color = _theme.GetCurrentColorScheme().foreground;
+            arrow.color = _theme.GetCurrentColorScheme().foreground;
         }
     }
 }
