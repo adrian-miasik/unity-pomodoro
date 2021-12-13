acb0e7269ef4d4dd98ebb486ed9087c4ad24c21f
Adrian Miasik <github@adrian-miasik.com>
2021-12-13 16:38:32 -0500

    Create WIP TwoChoiceDialog class

---
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/TwoChoiceDialog.cs
@@ -0,0 +1,16 @@
+using AdrianMiasik.Components.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AdrianMiasik.Components
+{
+    public class TwoChoiceDialog : MonoBehaviour
+    {
+        [SerializeField] private Image backgroundBox;
+        [SerializeField] private TMP_Text topLabel;
+        [SerializeField] private TMP_Text botLabel;
+        [SerializeField] private ClickButton cancel;
+        [SerializeField] private ClickButton submit;
+    }
+}
