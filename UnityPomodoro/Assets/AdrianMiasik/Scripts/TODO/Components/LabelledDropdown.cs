1e068ef0989b4e16b782248da9c660696453a8e7
Adrian Miasik <github@adrian-miasik.com>
2021-12-21 20:15:15 -0500

    Apply naming convention rename to LabelledDropdown

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/TODO/Components/LabelledDropdown.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/TODO/Components/LabelledDropdown.cs
@@ -9,34 +9,34 @@ namespace AdrianMiasik.Components
 {
     public class LabelledDropdown : MonoBehaviour, IColorHook
     {
-        [SerializeField] private TMP_Text label;
-        [SerializeField] private TMP_Dropdown dropdown;
-        [SerializeField] private Image containerOutline;
-        [SerializeField] private SVGImage arrow;
+        [SerializeField] private TMP_Text m_label;
+        [SerializeField] private TMP_Dropdown m_dropdown;
+        [SerializeField] private Image m_containerOutline;
+        [SerializeField] private SVGImage m_arrow;
 
         private PomodoroTimer timer;
     
-        public void Initialize(PomodoroTimer _timer)
+        public void Initialize(PomodoroTimer pomodoroTimer)
         {
-            timer = _timer;
+            timer = pomodoroTimer;
             timer.GetTheme().RegisterColorHook(this);
 
             SetDropdownValue(timer.GetDigitFormat());
         }
 
-        public void SetDropdownValue(int _selectionValue)
+        public void SetDropdownValue(int selectionValue)
         {
             // Set dropdown value to current digit format 
             // Note: This will trigger an OnValueChanged invoke
-            dropdown.value = _selectionValue;
+            m_dropdown.value = selectionValue;
         }
 
-        public void ColorUpdate(Theme _theme)
+        public void ColorUpdate(Theme theme)
         {
-            label.color = _theme.GetCurrentColorScheme().m_foreground;
-            dropdown.captionText.color = _theme.GetCurrentColorScheme().m_foreground;
-            containerOutline.color = _theme.GetCurrentColorScheme().m_backgroundHighlight;
-            arrow.color = _theme.GetCurrentColorScheme().m_foreground;
+            m_label.color = theme.GetCurrentColorScheme().m_foreground;
+            m_dropdown.captionText.color = theme.GetCurrentColorScheme().m_foreground;
+            m_containerOutline.color = theme.GetCurrentColorScheme().m_backgroundHighlight;
+            m_arrow.color = theme.GetCurrentColorScheme().m_foreground;
         }
     }
 }
