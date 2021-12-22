088de41d7ad78e55ae4bbd908764f873e0c6401b
Adrian Miasik <github@adrian-miasik.com>
2021-12-21 20:32:00 -0500

    Apply naming convention rename to ThemeSlider

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/TODO/Components/ThemeSlider.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/TODO/Components/ThemeSlider.cs
@@ -7,40 +7,40 @@ namespace AdrianMiasik.Components
 {
     public class ThemeSlider : ThemeElement
     {
-        [SerializeField] private BooleanSlider toggle;
-        [SerializeField] private TMP_Text label;
+        [SerializeField] private BooleanSlider m_toggle;
+        [SerializeField] private TMP_Text m_label;
         
-        public void Initialize(PomodoroTimer _timer, bool _state)
+        public void Initialize(PomodoroTimer pomodoroTimer, bool state)
         {
             // Register theme element
-            base.Initialize(_timer);
+            base.Initialize(pomodoroTimer);
             
-            toggle.OverrideDotColor(_timer.GetTheme().GetCurrentColorScheme().m_foreground);
-            toggle.Initialize(_timer, _state);
+            m_toggle.OverrideDotColor(pomodoroTimer.GetTheme().GetCurrentColorScheme().m_foreground);
+            m_toggle.Initialize(pomodoroTimer, state);
         }
 
-        public override void ColorUpdate(Theme _theme)
+        public override void ColorUpdate(Theme theme)
         {
-            toggle.ColorUpdate(_theme);
-            label.color = _theme.GetCurrentColorScheme().m_foreground;
-            label.text = timer.GetTheme().m_isLightModeOn ? "Dark" : "Light";
+            m_toggle.ColorUpdate(theme);
+            m_label.color = theme.GetCurrentColorScheme().m_foreground;
+            m_label.text = timer.GetTheme().m_isLightModeOn ? "Dark" : "Light";
             // TODO: Change toggle dot image to a moon for dark mode
         }
 
         // Piper methods
-        public void OverrideFalseColor(Color _color)
+        public void OverrideFalseColor(Color color)
         {
-            toggle.OverrideFalseColor(_color);
+            m_toggle.OverrideFalseColor(color);
         }
 
-        public void OverrideTrueColor(Color _color)
+        public void OverrideTrueColor(Color color)
         {
-            toggle.OverrideTrueColor(_color);
+            m_toggle.OverrideTrueColor(color);
         }
 
         public void Interact()
         {
-            toggle.OnPointerClick(null);
+            m_toggle.OnPointerClick(null);
         }
     }
 }
