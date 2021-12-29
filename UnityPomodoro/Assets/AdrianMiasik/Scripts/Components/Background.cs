29bac2fc33ab658d42290b7f276464ddf23069ee
Adrian Miasik <github@adrian-miasik.com>
2021-12-29 17:14:19 -0500

    Implement ThemeElement to Background

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Background.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Background.cs
@@ -1,27 +1,21 @@
-using AdrianMiasik.Interfaces;
+using AdrianMiasik.Components.Core;
 using AdrianMiasik.ScriptableObjects;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace AdrianMiasik.Components
 {
-    public class Background : MonoBehaviour, IColorHook
+    public class Background : ThemeElement
     {
         [SerializeField] private Image m_background;
         [SerializeField] private Selectable m_selectable;
-
-        public void Initialize(PomodoroTimer pomodoroTimer)
-        {
-            pomodoroTimer.GetTheme().RegisterColorHook(this);
-            ColorUpdate(pomodoroTimer.GetTheme());
-        }
         
         public void Select()
         {
             m_selectable.Select();
         }
 
-        public void ColorUpdate(Theme theme)
+        public override void ColorUpdate(Theme theme)
         {
             m_background.color = theme.GetCurrentColorScheme().m_background;
         }
