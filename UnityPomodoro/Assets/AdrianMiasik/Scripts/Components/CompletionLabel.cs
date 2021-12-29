f34ebe2e19f21a13019856b651ab73cfee8bd813
Adrian Miasik <github@adrian-miasik.com>
2021-12-29 17:15:01 -0500

    Implement ThemeElement to CompletionLabel

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CompletionLabel.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CompletionLabel.cs
@@ -1,22 +1,16 @@
 using System.Collections.Generic;
-using AdrianMiasik.Interfaces;
+using AdrianMiasik.Components.Core;
 using AdrianMiasik.ScriptableObjects;
 using TMPro;
 using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
-    public class CompletionLabel : MonoBehaviour, IColorHook
+    public class CompletionLabel : ThemeElement
     {
         [SerializeField] private List<TMP_Text> m_labels = new List<TMP_Text>();
-
-        public void Initialize(PomodoroTimer pomodoroTimer)
-        {
-            pomodoroTimer.GetTheme().RegisterColorHook(this);
-            ColorUpdate(pomodoroTimer.GetTheme());
-        }
-
-        public void ColorUpdate(Theme theme)
+        
+        public override void ColorUpdate(Theme theme)
         {
             foreach (TMP_Text text in m_labels)
             {
