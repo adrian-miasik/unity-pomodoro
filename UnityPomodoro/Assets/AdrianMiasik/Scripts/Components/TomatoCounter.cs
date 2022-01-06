c26bf088029c474134c5bc9189037ed0a35603eb
Adrian Miasik <github@adrian-miasik.com>
2022-01-05 19:02:17 -0500

    Stub in Tomato into TomatoCounter

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/TomatoCounter.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/TomatoCounter.cs
@@ -7,10 +7,14 @@ namespace AdrianMiasik.Components
     public class TomatoCounter : ThemeElement
     { 
         [SerializeField] private HorizontalLayoutGroup m_horizontal;
-
+        [SerializeField] private Tomato m_tomatoPrefab;
+        
         public override void Initialize(PomodoroTimer pomodoroTimer, bool updateColors = true)
         {
             base.Initialize(pomodoroTimer, updateColors);
+            
+            // TODO: Create tomatoes
+            // TODO: Initialize tomatoes
 
             Debug.Log("Initialized tomato counter");
         }
