479e27270acf0b5c99f83f04a3cd799c324c00c8
Adrian Miasik <github@adrian-miasik.com>
2022-01-06 18:53:05 -0500

    Create Complete and Reset methods for Tomato (visuals)

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Tomato.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Tomato.cs
@@ -1,10 +1,25 @@
 using AdrianMiasik.Components.Core;
 using AdrianMiasik.ScriptableObjects;
+using Unity.VectorGraphics;
+using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
     public class Tomato : ThemeElement
     {
+        [Header("References")]
+        [SerializeField] private SVGImage m_body;
+        [SerializeField] private SVGImage m_stem;
+
+        [Header("Assets")] 
+        [Header("Hollow")] 
+        [SerializeField] private Sprite m_hollowCroppedBody;
+        [SerializeField] private Sprite m_hollowFullBody;
+
+        [Header("Complete")]
+        [SerializeField] private Sprite m_croppedBody;
+        [SerializeField] private Sprite m_fullBody;
+        
         public override void Initialize(PomodoroTimer pomodoroTimer, bool updateColors = true)
         {
             base.Initialize(pomodoroTimer, updateColors);
@@ -13,6 +28,20 @@ namespace AdrianMiasik.Components
         public override void ColorUpdate(Theme theme)
         {
             base.ColorUpdate(theme);
+        }
+
+        [ContextMenu("Complete")]
+        public void Complete()
+        {
+            m_body.sprite = m_croppedBody;
+            Debug.Log("Tomato completed!");
+        }
+
+        [ContextMenu("Reset")]
+        public new void Reset()
+        {
+            m_body.sprite = m_hollowCroppedBody;
+            Debug.Log("Tomato reset!");
         }
     }
 }
