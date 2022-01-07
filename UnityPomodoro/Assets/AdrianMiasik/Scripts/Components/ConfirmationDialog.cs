39190d13f9e57a2cc1a711eea351787af38636c0
Adrian Miasik <github@adrian-miasik.com>
2022-01-07 16:09:06 -0500

    Add TODO to confirmation dialog

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/ConfirmationDialog.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/ConfirmationDialog.cs
@@ -24,6 +24,7 @@ namespace AdrianMiasik.Components
         private Action onCancel;
         private Action onSubmit;
         
+        // TODO: Add text arguments
         public void Initialize(PomodoroTimer pomodoroTimer, Action submit, Action cancel)
         {
             base.Initialize(pomodoroTimer);
