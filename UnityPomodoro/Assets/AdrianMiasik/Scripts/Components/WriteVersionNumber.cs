c8121f9cf038c01313f35f1117de4a874a10fd1b
Adrian Miasik <github@adrian-miasik.com>
2022-01-12 18:38:51 -0500

    Add documentation/XML comments to WriteNumberVersion

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/WriteVersionNumber.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/WriteVersionNumber.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// Writes the current unity application player settings bundle number to the referenced text field.
+    /// </summary>
     public class WriteVersionNumber : MonoBehaviour
     {
         [SerializeField] private TMP_Text m_text;
@@ -13,6 +16,10 @@ namespace AdrianMiasik.Components
             m_text.text = m_prefixString + Application.version;
         }
 
+        /// <summary>
+        /// Sets the color of the referenced text to the provided color. <param name="newColor"></param>
+        /// </summary>
+        /// <param name="newColor">The color you want the referenced text to change to.</param>
         public void SetTextColor(Color newColor)
         {
             m_text.color = newColor;
