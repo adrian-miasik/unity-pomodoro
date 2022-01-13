b775796a8025c67417a2c721291e7d468c19f4dd
Adrian Miasik <github@adrian-miasik.com>
2022-01-13 16:48:47 -0500

    Add XML documentation to CompletionLabel

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CompletionLabel.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/CompletionLabel.cs
@@ -1,15 +1,24 @@
 using System.Collections.Generic;
 using AdrianMiasik.Components.Core;
+using AdrianMiasik.Interfaces;
 using AdrianMiasik.ScriptableObjects;
 using TMPro;
 using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// A <see cref="ThemeElement"/> used primarily to update the color of the referenced
+    /// "timer completed" labels via <see cref="IColorHook"/> ColorUpdate();
+    /// </summary>
     public class CompletionLabel : ThemeElement
     {
         [SerializeField] private List<TMP_Text> m_labels = new List<TMP_Text>();
         
+        /// <summary>
+        /// Applies our <see cref="Theme"/> changes to our referenced components when necessary.
+        /// </summary>
+        /// <param name="theme">The theme to apply on our referenced components.</param>
         public override void ColorUpdate(Theme theme)
         {
             foreach (TMP_Text text in m_labels)
