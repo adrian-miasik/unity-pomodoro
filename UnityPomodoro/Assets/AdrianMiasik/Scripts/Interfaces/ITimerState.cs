77a00e3caa7331f22d73dcd59230fded53995c1e
Adrian Miasik <github@adrian-miasik.com>
2022-01-14 01:27:21 -0500

    Add XML summary to ITimerState interface

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Interfaces/ITimerState.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Interfaces/ITimerState.cs
@@ -4,6 +4,11 @@ namespace AdrianMiasik.Interfaces
 {
     public interface ITimerState
     {
+        /// <summary>
+        /// Invoked when the timers current state changes.
+        /// </summary>
+        /// <param name="state">The new timer state</param>
+        /// <param name="theme">The current theme</param>
         public void StateUpdate(PomodoroTimer.States state, Theme theme);
     }
 }
