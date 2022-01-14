c96f4f9e4768377d31e4f4bbeef1c80c3ebe4242
Adrian Miasik <github@adrian-miasik.com>
2022-01-14 01:47:31 -0500

    Add XML documentation to UPIcon

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/UPIcon.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/UPIcon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// An SVG image component that flips between two sprites. One for each theme (light / dark).
+    /// </summary>
     public class UPIcon : MonoBehaviour
     {
         [SerializeField] private SVGImage m_icon;
@@ -11,6 +14,10 @@ namespace AdrianMiasik.Components
         [SerializeField] private Sprite m_lightSprite;
         [SerializeField] private Sprite m_darkSprite;
 
+        /// <summary>
+        /// Applies our <see cref="Theme"/> changes to our referenced components when necessary.
+        /// </summary>
+        /// <param name="theme">The theme to apply on our referenced components.</param>
         public void ColorUpdate(Theme theme)
         {
             m_icon.sprite = theme.m_darkMode ? m_darkSprite : m_lightSprite;
