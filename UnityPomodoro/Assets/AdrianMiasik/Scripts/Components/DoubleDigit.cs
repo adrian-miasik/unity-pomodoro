fb2faf886e7868b64de952d87a8f329c3b99c8ae
Adrian Miasik <github@adrian-miasik.com>
2022-01-14 00:45:01 -0500

    Add XML documentation to DoubleDigit

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/DoubleDigit.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/DoubleDigit.cs
@@ -9,6 +9,12 @@ using UnityEngine.UI;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// A <see cref="ThemeElement"/> used as an interactable/lockable input field that's designed to hold two numerical
+    /// values. Can be interacted by the user via the increment / decrement arrows.
+    /// Intended to be used within a <see cref="DigitFormat"/>.
+    /// <remarks>Has two layers of selections. One for itself, another for the input field.</remarks>
+    /// </summary>
     public class DoubleDigit : ThemeElement, ISelectHandler, IPointerClickHandler, ISubmitHandler
     {
         [Header("References")] 
@@ -25,6 +31,9 @@ namespace AdrianMiasik.Components
         [SerializeField] private Animation m_pulseWobble;
         [SerializeField] private Animation m_tick;
         
+        /// <summary>
+        /// The <see cref="DigitFormat.Digits"/> these two numerical are suppose to represent.
+        /// </summary>
         [HideInInspector] public DigitFormat.Digits m_digit;
         
         // Cache
@@ -50,11 +59,24 @@ namespace AdrianMiasik.Components
         // Shaders
         private Material instanceMaterial;
         private static readonly int SquircleColor = Shader.PropertyToID("Color_297012532bf444df807f8743bdb7e4fd");
-
-        // Unity Events
+        
+        /// <summary>
+        /// Invoked when this <see cref="DoubleDigit"/> is selected.
+        /// </summary>
         public UnityEvent m_onSelection;
-        public UnityEvent m_onDigitChange; // Invoked only when timer is running
+        
+        /// <summary>
+        /// Invoked when this value changes to something different.
+        /// <remarks>Invoked only when the timer is running.</remarks>
+        /// </summary>
+        public UnityEvent m_onDigitChange;
 
+        /// <summary>
+        /// Disables TextMeshPro's run-time caret, and hides the associated increment / decrement arrows.
+        /// </summary>
+        /// <param name="pomodoroTimer"></param>
+        /// <param name="digitFormat"></param>
+        /// <param name="digit"></param>
         public void Initialize(PomodoroTimer pomodoroTimer, DigitFormat digitFormat, DigitFormat.Digits digit)
         {
             base.Initialize(pomodoroTimer);
@@ -72,6 +94,11 @@ namespace AdrianMiasik.Components
             HideArrows();
         }
 
+        /// <summary>
+        /// Sets the inputs fields current value to the provided one, checks if arrows need to be visible/hidden, and
+        /// stops our pulse wobble animation if one is playing.
+        /// </summary>
+        /// <param name="value">What value should this <see cref="DoubleDigit"/> display?</param>
         public void UpdateVisuals(int value)
         {
             SetTextLabel(value);
@@ -179,7 +206,10 @@ namespace AdrianMiasik.Components
             isColorAnimating = true;
         }
 
-        // Unity Event
+        /// <summary>
+        /// Plays our tick animation.
+        /// <remarks>Used as a <see cref="UnityEvent"/> onValueChanged.</remarks>
+        /// </summary>
         public void PlayTickAnimation()
         {
             // Prevent milliseconds from animating on tick (due to how fast it ticks)
@@ -192,28 +222,42 @@ namespace AdrianMiasik.Components
             m_tick.Play();
         }
 
+        /// <summary>
+        /// Is our tick animation currently being played?
+        /// </summary>
+        /// <returns></returns>
         public bool IsTickAnimationPlaying()
         {
             return m_tick.isPlaying;
         }
 
+        /// <summary>
+        /// Resets our input field's default viewport positions & anchors.
+        /// </summary>
         public void ResetTextPosition()
         {
             m_input.textComponent.rectTransform.anchorMin = Vector2.zero;
             m_input.textComponent.rectTransform.anchorMax = Vector2.one;
         }
         
+        /// <summary>
+        /// Returns the input fields text.
+        /// </summary>
+        /// <returns></returns>
         public string GetDigitsLabel()
         {
             return m_input.text;
         }
         
+        /// <summary>
+        /// Makes the increment & decrement arrow hidden.
+        /// </summary>
         public void HideArrows()
         {
             m_upArrow.Hide();
             m_downArrow.Hide();
         }
-
+        
         private void ShowArrows()
         {
             if (format != null)
@@ -230,18 +274,28 @@ namespace AdrianMiasik.Components
             }
         }
 
+        /// <summary>
+        /// Prevents the input field from being interacted with.
+        /// </summary>
         public void Lock()
         {
             isInteractable = false;
             m_input.interactable = false;
         }
 
+        /// <summary>
+        /// Allows our input field to be interacted with.
+        /// </summary>
         public void Unlock()
         {
             isInteractable = true;
             m_input.interactable = true;
         }
         
+        /// <summary>
+        /// Sets the input field text to the provided color.
+        /// </summary>
+        /// <param name="newColor">The color you want the input fields text to be.</param>
         public void SetTextColor(Color newColor)
         {
             m_input.textComponent.color = newColor;
@@ -250,7 +304,7 @@ namespace AdrianMiasik.Components
         /// <summary>
         /// Sets the digit data to the provided input.
         /// This is different from the current visuals of this digit.
-        /// If you are looking to modify the digit visual value <see cref="SetTextLabel"/>
+        /// If you are looking to modify the digit visual value see <see cref="SetTextLabel"/>.
         /// </summary>
         /// <param name="value"></param>
         public void SetValue(int value)
@@ -264,8 +318,8 @@ namespace AdrianMiasik.Components
         /// <summary>
         /// Sets the digit label to the provided input.
         /// This is different from setting the digit time.
-        /// If you are looking to modify the users set/edited time <see cref="SetValue"/>.
-        /// Also if you are looking to update arrows as well as the label <seealso cref="UpdateVisuals"/>
+        /// If you are looking to modify the users set/edited time see <see cref="SetValue"/>.
+        /// Also if you are looking to update arrows as well as the label see <seealso cref="UpdateVisuals"/>.
         /// </summary>
         /// <param name="value"></param>
         public void SetTextLabel(int value)
@@ -283,6 +337,10 @@ namespace AdrianMiasik.Components
             }
         }
         
+        /// <summary>
+        /// Add one to this digit. (+1)
+        /// <remarks>Used as a UnityEvent on our (top) increment arrow.</remarks>
+        /// </summary>
         public void IncrementOne()
         {
             if (format == null) 
@@ -294,6 +352,9 @@ namespace AdrianMiasik.Components
             accumulatedSelectionTime = 0;
         }
 
+        /// <summary>
+        /// Removes one from this digit. (-1)
+        /// </summary>
         public void DecrementOne()
         {
             if (format == null) 
@@ -328,11 +389,21 @@ namespace AdrianMiasik.Components
             }
         }
 
+        /// <summary>
+        /// Highlights this <see cref="DoubleDigit"/> to display it's arrows and set's it's background color,
+        /// but does not set this as the <see cref="PomodoroTimer"/>'s digit selection. If you'd like to do both
+        /// see <see cref="OnSelect"/>
+        /// </summary>
         public void Highlight()
         {
             OnSelect(false);
         }
         
+        /// <summary>
+        /// Highlights this <see cref="DoubleDigit"/> to display it's arrows and set's it's background color,
+        /// and set this <see cref="DoubleDigit"/> as the <see cref="PomodoroTimer"/>'s current selection.
+        /// </summary>
+        /// <param name="eventData"></param>
         public void OnSelect(BaseEventData eventData)
         {
             OnSelect(true);
@@ -359,6 +430,9 @@ namespace AdrianMiasik.Components
             m_onSelection.Invoke();
         }
 
+        /// <summary>
+        /// De-Highlights this <see cref="DoubleDigit"/> to hide it's arrows, and reset it's background.
+        /// </summary>
         public void Deselect()
         {
             isSelected = false;
@@ -372,7 +446,9 @@ namespace AdrianMiasik.Components
             m_input.DeactivateInputField();
         }
 
-        // Unity Event
+        /// <summary>
+        /// <remarks>Used as a <see cref="UnityEvent"/> on our input field.</remarks>
+        /// </summary>
         public void SetValueEndEdit()
         {
             m_input.text = string.IsNullOrEmpty(m_input.text) ? "00" : m_input.text;
@@ -380,6 +456,10 @@ namespace AdrianMiasik.Components
             UpdateVisuals(format.GetDigitValue(m_digit));
         }
 
+        /// <summary>
+        /// Deselect input field and select ourself only. (So user doesn't interact with the input field but our
+        /// arrows instead)
+        /// </summary>
         public void DeselectInput()
         {
             // Disable caret selection
@@ -392,6 +472,10 @@ namespace AdrianMiasik.Components
             m_selectable.Select();
         }
 
+        /// <summary>
+        /// Selects this selectable, or the input fields selectable.
+        /// </summary>
+        /// <param name="eventData"></param>
         public void OnPointerClick(PointerEventData eventData)
         {
             // First click is reserved for digit selection, second click is for editing the input field
@@ -408,6 +492,10 @@ namespace AdrianMiasik.Components
             }
         }
 
+        /// <summary>
+        /// Interacts with this <see cref="DoubleDigit"/>.
+        /// </summary>
+        /// <param name="eventData"></param>
         public void OnSubmit(BaseEventData eventData)
         {
             if (!caret.raycastTarget)
@@ -421,7 +509,11 @@ namespace AdrianMiasik.Components
                 m_input.DeactivateInputField(true);
             }
         }
-
+        
+        /// <summary>
+        /// Applies our <see cref="Theme"/> changes to our referenced components when necessary.
+        /// </summary>
+        /// <param name="theme">The theme to apply on our referenced components.</param>
         public override void ColorUpdate(Theme theme)
         {
             ColorScheme currentColors = theme.GetCurrentColorScheme();
@@ -449,15 +541,13 @@ namespace AdrianMiasik.Components
             SetTextColor(currentColors.m_foreground);
         }
 
+        /// <summary>
+        /// Returns our <see cref="Selectable"/> component.
+        /// </summary>
+        /// <returns></returns>
         public Selectable GetSelectable()
         {
             return m_selectable;
-        }
-
-        public void SetTextScale(float scale = 1.55f)
-        {
-            m_input.textComponent.gameObject.transform.localScale = Vector3.one * scale;
-            caret.gameObject.transform.localScale = Vector3.one * scale;
         }
     }
 }
