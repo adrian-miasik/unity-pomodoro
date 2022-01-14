e15b04ba650f35e68a48fcf0927a9dcc850e2b3f
Adrian Miasik <github@adrian-miasik.com>
2022-01-14 00:44:51 -0500

    Add XML documentation to DigitSeparator

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/DigitSeparator.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/DigitSeparator.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// A single <see cref="TMP_Text"/> component used to hold reference and set it's color.
+    /// </summary>
     public class DigitSeparator : MonoBehaviour
     {
         [SerializeField] private TMP_Text m_separator;
 
+        /// <summary>
+        /// Sets the separator's color to the provided color.
+        /// </summary>
+        /// <param name="newColor">The color you want this separator to be.</param>
         public void SetSeparatorColor(Color newColor)
         {
             m_separator.color = newColor;
