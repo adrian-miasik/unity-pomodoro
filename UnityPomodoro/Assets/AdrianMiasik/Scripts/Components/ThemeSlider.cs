3ff632f11ef24419ced45b454d65856c2a9080cc
Adrian Miasik <github@adrian-miasik.com>
2022-01-14 02:02:08 -0500

    Add XML documentation to ThemeSlider

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/ThemeSlider.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/ThemeSlider.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 namespace AdrianMiasik.Components
 {
+    /// <summary>
+    /// A <see cref="ThemeElement"/> boolean slider with a label. Intended for the theme toggle (light / dark).
+    /// </summary>
     public class ThemeSlider : ThemeElement
     {
         [SerializeField] private BooleanSlider m_toggle;
         [SerializeField] private TMP_Text m_label;
         
+        /// <summary>
+        /// Applies our <see cref="Theme"/> changes to our referenced components when necessary.
+        /// And changes our text label from depending on the current <see cref="Theme"/>.
+        /// </summary>
+        /// <param name="theme">The theme to apply on our referenced components.</param>
         public override void ColorUpdate(Theme theme)
         {
             m_toggle.OverrideDotColor(theme.GetCurrentColorScheme().m_foreground);
@@ -22,16 +30,27 @@ namespace AdrianMiasik.Components
         }
 
         // Piper methods
+        /// <summary>
+        /// What color should this slider be when `False`?
+        /// </summary>
+        /// <param name="color">The color you want the boolean slider background to be.</param>
         public void OverrideFalseColor(Color color)
         {
             m_toggle.OverrideFalseColor(color);
         }
 
+        /// <summary>
+        /// What color should this slider be when `True`?
+        /// </summary>
+        /// <param name="color"></param>
         public void OverrideTrueColor(Color color)
         {
             m_toggle.OverrideTrueColor(color);
         }
 
+        /// <summary>
+        /// Presses our theme slider toggle.
+        /// </summary>
         public void Interact()
         {
             m_toggle.OnPointerClick(null);
