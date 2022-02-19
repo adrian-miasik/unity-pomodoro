599415bdd29815d0a8ce608b513c40dd4eeca08f
Adrian Miasik <github@adrian-miasik.com>
2022-02-19 18:00:13 -0500

    Created WIP class SystemSettings

---
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Core/SystemSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdrianMiasik.Components.Core
+{
+    /// <summary>
+    /// Global settings for the application. (Not specific to a timer, but instead the entire application)
+    /// </summary>
+    [Serializable]
+    public class SystemSettings
+    {
+        // TODO: Implementation
+        // TODO: Option: Theme settings
+        // TODO: Option: Set mute out of focus
+    }
+}
