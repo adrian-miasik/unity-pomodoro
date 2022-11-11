fa837a41410e4358310b1ae90272b1f38795f5ea
Adrian Miasik <git@adrian-miasik.com>
2022-11-10 21:19:26 -0500

    Create a helper class DateTimeHelper
    Includes an extension method to trim milliseconds from a DateTime

---
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Core/Helpers/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdrianMiasik.Components.Core.Helpers
+{
+    /// <summary>
+    /// Helper methods for managing and dealing with DateTime
+    /// </summary>
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Extension method that removes the milliseconds from the DateTime in question.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime TrimMilliseconds(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute,
+                dateTime.Second, 0, dateTime.Kind);
+        }
+    }
+}
