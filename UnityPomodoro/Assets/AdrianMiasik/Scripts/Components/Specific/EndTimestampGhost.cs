b2eb7e83be793c6dbf459654f025709f65f65823
Adrian Miasik <git@adrian-miasik.com>
2023-01-09 18:07:55 -0500

    Modify EndTimestampGhost to bring you back to the timer page
    This way the user can return to the timer with one click instead of pressing two individual buttons: one for side main toggle, the second for the pomodoro timer.

--- UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Specific/EndTimestampGhost.cs
+++ UnityPomodoro/Assets/AdrianMiasik/Scripts/Components/Specific/EndTimestampGhost.cs
@@ -10,16 +10,29 @@ namespace AdrianMiasik.Components.Specific
     /// </summary>
     public class EndTimestampGhost : Ghost
     {
-        // Cache
-        private float fadeProgress;
-        
+        protected override void Update()
+        {
+            base.Update();
+            
+            // Prevent text buttons interactions when end timer ghost is invisible OR when we are already on the 
+            // main content page.
+            if (m_fadeProgress <= 0 || Timer.IsMainContentOpen())
+            {
+                m_textContainer.blocksRaycasts = false;
+            }
+            else
+            {
+                m_textContainer.blocksRaycasts = true;
+            }
+        }
+
         public override void FadeIn(bool instantly = false)
         {
             CalculateEndTime();
             
             base.FadeIn(instantly);
         }
-        
+
         private void CalculateEndTime()
         {
             // Get current current remaining time
@@ -41,6 +54,17 @@ namespace AdrianMiasik.Components.Specific
             
             // Remain visible depending on theme dark mode preference
             m_icon.ChangeColor(theme.GetCurrentColorScheme().m_foreground);
+        }
+
+        /// <summary>
+        /// Hooked up to the SVG icon button on the prefab.
+        /// </summary>
+        public void OnClick()
+        {
+            if (!Timer.IsMainContentOpen())
+            {
+                Timer.ShowMainContent();
+            }
         }
     }
 }
